8eed13fe90fad0b3ed6253407cfde6e0613924a0
msaelices <msaelices@gmail.com>
2018-01-04 02:04:08 +0100

    Fix script.

--- png2pdf.sh
+++ png2pdf.sh
@@ -1,2 +1,2 @@
-convert 1.png 2.png 3.png 4.png 5.png 6.png 7.png 8.png 9.png 10.png 11.png 12.png calendario.pdf
+convert 1.png 2.png 3.png 4.png 5.png 6.png 7.png 8.png 9.png 10.png 11.png 12.png calendar.pdf
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages -dCompressFonts=true -r150 -sOutputFile=calendar_optimized.pdf calendar.pdf
